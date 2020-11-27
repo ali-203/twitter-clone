472c96b501a457d2cadc1fdc12a2f255d6a8c71b
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-28 01:09:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Sidebar from "./Sidebar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
     <h1>Hey Clever Programmers... Let's build a Twitter Clone 🚀</h1>
-      
+    <Sidebar />
     </div>
   );
 }
