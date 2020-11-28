a063397c4225918ee09af67dd9023aed39b74342
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-28 05:02:12 +0500

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,14 @@
 import React from "react";
-import "./App.css";
+import "./Sidebar.css";
 import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from "./SidebarOption";
+
 
 function Sidebar() {
 return (
     <div className="sidebar">
     <TwitterIcon />
-    
+    <SidebarOption />
 
     </div>
 );
---
+++ src/SidebarOption.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./SidebarOption.css";
+
+
+function SidebarOption() {
+    return (
+        <div className="sidebarOption">
+        <h2>This is an option</h2>
+            
+        </div>
+    )
+}
+
+export default SidebarOption
