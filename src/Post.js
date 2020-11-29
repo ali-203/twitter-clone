8a53da2c6b05ec427eefdf017e62c0dc39766ef9
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-30 04:58:29 +0500

    update

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
 import "./Post.css";
+import { VerifiedUser } from "@material-ui/icons";
 
 function Post ({ displayName, username, verified, text, image, avatar})
   {
@@ -10,7 +11,18 @@ function Post ({ displayName, username, verified, text, image, avatar})
         <div className="post_avatar">
         <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/setting_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"></Avatar>
         </div>
-            
+        <div className="post_body">
+        <div className="post_header">
+            <div className="post_headerText">
+            <h3> Ali Raza{""}
+            <span>
+                <VerifiedUserIcon className="post_badge"></VerifiedUserIcon>
+            </span>
+             </h3>
+        </div>
+
+        </div>
+            </div>
         </div>
     );
     }
