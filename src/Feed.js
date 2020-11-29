792e0347916ca984d40a6ea8877c5804e7abe47c
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-11-30 00:20:22 +0500

    add css

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Feed.css";
+import TweetBox from "./TweetBox";
 
 function Feed() {
     return (
         <div className="feed_header">
         <h2>Home</h2>    
+        <TweetBox />
         </div>
     )
 }
