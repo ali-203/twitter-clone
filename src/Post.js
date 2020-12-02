0b24610bac842c9d438875986397af0d0c54a67c
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-03 03:07:36 +0500

    update

--- src/Post.js
+++ src/Post.js
@@ -1,35 +1,46 @@
 
-import React from "react";
-import { Avatar } from "@material-ui/core";
+import React, { forwardRef } from "react";
 import "./Post.css";
-import { VerifiedUserIcon } from "@material-ui/icons/VerifiedUser";
+import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
-function Post({ displayName, username, verified, text, image, avatar}) {
-  
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
     return (
-        <div className="post">
-        <div className="post_avatar">
-        <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/setting_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"></Avatar>
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar} />
         </div>
-        <div className="post_body">
-        <div className="post_header">
-            <div className="post_headerText">
-            <h3> Ali Raza{""}
-            <span>
-                <VerifiedUserIcon className="post_badge"/> @cleverqazi
-            </span>
-             </h3>
-        </div>
-        <div className="post_headerDescription">
-            <p>I challenge you to build a Twitter Clone with React !!! </p>
-        </div>
-
-        </div>
-        <img src="https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif"alt="" />
-        
+        <div className="post__body">
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />} @
+                  {username}
+                </span>
+              </h3>
             </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
+          </div>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
         </div>
+      </div>
     );
-    }
+  }
+);
 
 export default Post;
