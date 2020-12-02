2c9a87938721ae90f16a42537a7b4833261b2042
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-03 02:58:53 +0500

    update

--- src/Feed.js
+++ src/Feed.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import "./Feed.css";
+import React, { useState, useEffect } from "react";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
+import "./Feed.css";
+import db from "./firebase";
+import FlipMove from "react-flip-move";
+import { StarTwoTone } from "@material-ui/icons";
 
 function Feed() {
-    return (
-        <div className="feed">
-        <div className="feed_header">
-        <h2>Home</h2>
-        </div>    
-        <TweetBox />
-        <Post />
-        
-        </div>
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
     );
+  }, []);
+
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      <TweetBox />
+
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
 }
 
 export default Feed;
